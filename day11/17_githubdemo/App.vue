72374b6917493a67ba7c40824355b64eae706c6b
unknown <2469045657@qq.com>
2022-03-03 22:28:17 +0800

    devServerProxy02&&githubdemo

---
+++ day11/17_githubdemo/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="container">
+        <Search></Search>
+        <List></List>
+    </div>
+  </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import Search from './components/Search'
+    import List from './components/List'
+    export default {
+        name: "App",
+        components: {
+            Search,
+            List
+        }
+    };
+</script>
---
+++ day11/17_githubdemo/main.js
@@ -0,0 +1,18 @@
+// 引入vue
+import Vue from 'vue';
+// 引入app组件
+import App from './App.vue'
+
+//引入插件
+// import plugins from './plugins';
+
+//应用插件
+// Vue.use(plugins)
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    beforeCreate() {
+        Vue.prototype.$bus = this
+    }
+})
