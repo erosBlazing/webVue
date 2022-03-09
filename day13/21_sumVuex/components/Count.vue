5aaf8737015cae9e59f81bc6c0dad42c16d4a93a
unknown <2469045657@qq.com>
2022-03-09 21:23:12 +0800

    Vuex

---
+++ day13/21_sumVuex/components/Count.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <h1>当前求和为：{{this.$store.state.sum}}</h1>
+      <select v-model.number="number">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+      </select>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+      <button @click="incrementOdd">为奇数再加</button>
+      <button @click="incrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Count',
+        data() {
+            return {
+                number: 1
+            }
+        },
+        methods: {
+            increment() {
+                this.$store.dispatch('add', this.number)
+            },
+            decrement() {
+                this.$store.dispatch('jian', this.number)
+            },
+            incrementOdd() {
+                if(this.$store.state.sum % 2) {
+                    this.$store.dispatch('add', this.number)
+                }
+            },
+            incrementWait() {
+                setTimeout(() => {
+                    this.$store.dispatch('add', this.number)
+                }, 1000)
+            }
+        }
+    }
+</script>
+
+<style>
+    button {
+        margin-left: 5px;
+    }
+</style>
---
+++ day13/21_sumVuex/store/index.js
@@ -0,0 +1,39 @@
+//该文件用于创建Vuex最为核心的store
+
+import Vue from 'vue'
+
+import Vuex from 'vuex'
+
+//准备 actions 用于响应组件中的动作
+const actions = {
+    add(context, value) {
+        context.commit('ADD', value)
+    },
+    jian(context, value) {
+        context.commit('JIAN', value)
+    }
+}
+
+//准备 mutations 用于操作数据
+const mutations = {
+    ADD(state, value) {
+        state.sum += value
+    },
+    JIAN(state, value) {
+        state.sum -= value
+    }
+}
+
+//准备 state 用于存储数据
+const state = {
+    sum: 0
+}
+
+Vue.use(Vuex)
+
+//创建并导出 store
+export default new Vuex.Store({
+    actions,
+    mutations,
+    state
+})
